92dd00087a250ec4e7b872a6cae1059a97d5d80d
habachiemna <habachiemnap4@gmail.com>
2021-06-22 17:48:36 +0200

    mon commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function todolist(){
+    var item = document.getElementById("todoInput").value 
+    var text = document.createTextNode(item)
+    var newItem = document.createElement("li")
+    newItem.appendChild(text)
+    document.getElementById("todolist").appendChild(newItem) 
+}
